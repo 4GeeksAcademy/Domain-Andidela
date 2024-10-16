14a8bf28ea63bd2a42b86a431ed9d6f3a684eda3
Andidela <andidela83@gmail.com>
2024-10-16 20:38:43 +0000

    Proyecto Finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  for (let i = 0; i < pronoun.length; i++) {
+    const pronombre = pronoun[i];
+    for (let j = 0; j < adj.length; j++) {
+      const adjetivo = adj[j];
+      for (let k = 0; k < noun.length; k++) {
+        const sustantivo = noun[k];
+        console.log(pronombre + adjetivo + sustantivo);
+      }
+    }
+  }
 };
